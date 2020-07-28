9cee7404a3f6a8c657eafc363f3b2bafa9deff8b
marissa-mayes <marissa.mayes1289@gmail.com>
2020-07-27 21:41:07 -0500

    created save button and save to local storage

--- assests/workday.js
+++ assests/workday.js
@@ -2,24 +2,19 @@
 //var $saveButton = $(".btn");
 var $currentDay =$("#currentDay");
 var CurrentTime = getCurrentTime()
-$ (document).ready (function(){
-    getTodaysDate()
-});
-
-
 var $planner  =$("#plannerContainer")
-            var a = moment().format("LLLL");
-      console.log(a);
-      var timelist=["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM",]
-      var scheduleData=[
-        {
-        time:"9AM",
-        description: "gotta do that thing",
-      
-        
-      },
+var a = moment().format("LLLL");
+console.log(a);
+var timelist=["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM",]
+var scheduleData=[
+  {
+    time:"9AM",
+    description: "gotta do that thing",
     
-        {
+    
+  },
+  
+  {
         time:"10AM",
         description: "gotta do that other thing"
       },
@@ -76,11 +71,12 @@ var $planner  =$("#plannerContainer")
         var momentStub= moment(CurrentTime, "ha")
         console.log(momentStub)
         scheduleData.forEach(events=>{
-          console.log(events)
           var row= $("<div class='row time-block'>")
           
           var eventTime= $(`<div class='hour'>`).text(events.time)
+          console.log(events.time)
           var localDescription= localStorage.getItem(events.time)
+          console.log(localDescription)
           var eventDesc= $(`<textarea class='dexcription'>`).text(localDescription||events.description)
           var momentNow= moment(events.time, "ha")
          
@@ -95,11 +91,15 @@ var $planner  =$("#plannerContainer")
             eventDesc.addClass("present")
 
           }
-          var buttonDiv =$("<div>");
-          var inputDiv= $("<div>");
-          var button = $("<button>");
-          var input = $("<input>");
-          row.append(eventTime, eventDesc)
+          let $saveButton = $("<i>");
+      $saveButton.attr("id",`saveid-${events.time}`);
+      $saveButton.attr("save-id",events.time);//data
+      $saveButton.attr("class","far fa-save listener");
+          // var buttonDiv =$("<div>");
+          // var inputDiv= $("<div>");
+          // var button = $("<button>");
+          // var input = $("<input>");
+          row.append(eventTime, eventDesc, $saveButton)
           $planner.append(row)
           //$("#button").click(function(){
 
@@ -111,26 +111,38 @@ var $planner  =$("#plannerContainer")
 
         }
 
+        
+        
+        
+        //buttonDiv.append(button);
+        //inputDiv.append(input);
+        $(document).ready(function(){
+          getTodaysDate()
+          renderData()
+          //$(document).on('click', ".listener", event=>{//this attaches listener to the document so that dynamic elements can be added later
+          $(".listener").on('click',  function(event){//this attaches listener to the document so that dynamic elements can be added later
+              event.preventDefault()
+              console.log(this)//vanilla js html dom element
+              let key= $(this).attr("save-id")
+              let value= $(this).siblings("textarea").val()
+              localStorage.setItem(key,value)
 
+          })
+        })
+        
+        
+    //   let $rowDiv= $("<div>");
+    //   $rowDiv.addClass("row");
+    //   let $columnSave = $("<div>");
+    //   $columnSave.addClass("col-md-1");
+    //   let index = "save-id";
+    //   let $SaveDiv = $("<div>");
+    //   $SaveDiv.addClass("col-md-1");
+    //   let $saveButton = $("<i>");
+    //   $saveButton.attr("id",`saveid-${index}`);
+    //   $saveButton.attr("save-id",index);
+    //   $saveButton.attr("class","far fa-save");
+    //   $rowDiv.append($columnSave);
+    //   $columnSave.append($saveButton);
 
-
-
-          //buttonDiv.append(button);
-          //inputDiv.append(input);
-  
-      renderData()
-      let $rowDiv= $("<div>");
-      $rowDiv.addClass("row");
-      let $columnSave = $("<div>");
-      $columnSave.addClass("col-md-1");
-    let index = "save-id";
-      let $SaveDiv = $("<div>");
-      $SaveDiv.addClass("col-md-1");
-      let $saveButton = $("<i>");
-      $saveButton.attr("id",`saveid-${index}`);
-      $saveButton.attr("save-id",index);
-      $saveButton.attr("class","far fa-save");
-      $rowDiv.append($columnSave);
-      $columnSave.append($saveButton);
-
-    let $index = $(this).attr("save-id");
+    // let $index = $(this).attr("save-id");
